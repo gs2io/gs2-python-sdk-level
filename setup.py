bc9eacd3210c036d3943b8f50e99622746ddc41b
Haruhiko Imatake <imatake_haruhiko@gs2.io>
2018-10-18 13:54:12 +0900

    ライブラリバージョンを更新

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='gs2-python-sdk-level',
-    version='1.1.10',
+    version='1.3.1',
     package_dir={'': 'src'},
     packages=[
         "",
